cb4b99c388bc7dba410e8af2a835eba90817fbe3
TomSawyer2 <6272867@qq.com>
2022-12-03 21:17:23 +0800

    feat: change sh

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,11 @@ FROM alpine:edge
 
 RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.tuna.tsinghua.edu.cn/g' /etc/apk/repositories
 # 安装openjdk17
-RUN apk add --no-cache openjdk17
-# 安装可以执行xvfb-run的虚拟显示器
-RUN apk add --no-cache xvfb-run
+RUN apk add --no-cache openjdk17 xvfb-run
 
 RUN mkdir -p /home/game
 WORKDIR /home/game
 COPY . /home/game
 
-# 启动容器的方式 docker run --env totalRound=100 outputFilename=output
+# 启动容器的方式 docker run --env totalRound=100 upperOutputFilename=upperOutput downOutputFilename=downOutput
 ENTRYPOINT ["sh", "init.sh"]
--- init.sh
+++ init.sh
@@ -1,8 +1,28 @@
 #! /bin/bash
-# This script is used to start the game
+# This script is used to monitor the game
 totalRound=$totalRound
-outputFilename=$outputFilename
+upperOutputFilename=$upperOutputFilename
+downOutputFilename=$downOutputFilename
+
+# 将code文件夹中的codeAHoney.java复制到当前目录，并重命名为HoneyBee.java
+cp code/codeAHoney.java HoneyBee.java
+# 将code文件夹中的codeBHornet.java复制到当前目录，并重命名为Hornet.java
+cp code/codeBHornet.java Hornet.java
 # 使用javac编译目录下所有java文件
 javac -encoding UTF-8 -d . *.java
 # java BeeFarming并传入totalRound和outputFilename
-xvfb-run -a java BeeFarming totalRound=$totalRound outputFilename=$outputFilename
+xvfb-run -a java BeeFarming totalRound=$totalRound outputFilename=$upperOutputFilename
+
+# 删除编译后的class文件
+rm *.class
+# 删除HoneyBee.java和Hornet.java
+rm HoneyBee.java
+rm Hornet.java
+# 将code文件夹中的codeBHoney.java复制到当前目录，并重命名为HoneyBee.java
+cp code/codeBHoney.java HoneyBee.java
+# 将code文件夹中的codeAHornet.java复制到当前目录，并重命名为Hornet.java
+cp code/codeAHornet.java Hornet.java
+# 使用javac编译目录下所有java文件
+javac -encoding UTF-8 -d . *.java
+# java BeeFarming并传入totalRound和outputFilename
+xvfb-run -a java BeeFarming totalRound=$totalRound outputFilename=$downOutputFilename
